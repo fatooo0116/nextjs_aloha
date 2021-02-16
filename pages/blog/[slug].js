63f63ca3de47ec6c43f1f30d602410c5c5c969e7
fatooo0116@gmail.com <cbsi940424>
2021-02-16 23:22:47 +0800

    change bug

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,27 +2,16 @@ import styles from '../../styles/Home.module.css'
 // import Header from '/tpl/_header'
 // import Footer from '/tpl/_footer'
 
-import {getAllPostsForHome} from '../../lib/api'
+// import {getAllPostsForHome} from '../../lib/api'
 
 
 export default function posts(allPosts) {
 
-    
-
   return (
     <div className={styles.container}>
-
-
-    
-       
-
         <main>
             <h2>POST</h2>
-          
         </main>
-
-    
-
     </div>
   )
 }

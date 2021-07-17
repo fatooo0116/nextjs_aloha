2a1d7eea0425a603afb2963ad4e7a70ef49244ba
fatooo0116 <fatooo0116@gmail.com>
2021-07-17 21:41:01 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -13,24 +13,35 @@ export default function Home(props) {
       <Header />      
         <main className="page_contents">
 
-          <div id="intro" class="works-intro">
-            <div class="works-intro__inner u-inner">
-              <div class="works-intro__txt-wrap txtWrap">
-                <h1 class="works-intro__head tit c-head01 u-poppins">works</h1>
-                <ul class="works-intro__tab tab">
-                  <li class="works-intro__item item u-poppins is-current"><a class="works-intro__link" href="https://www.m-hand.co.jp/works/">all</a></li>
-                  <li class="works-intro__item item u-poppins"><a class="works-intro__link" href="https://www.m-hand.co.jp/works/cat/service">SERVICE</a></li>
-                  <li class="works-intro__item item u-poppins"><a class="works-intro__link" href="https://www.m-hand.co.jp/works/cat/corporate">CORPORATE</a></li>
-                  <li class="works-intro__item item u-poppins"><a class="works-intro__link" href="https://www.m-hand.co.jp/works/cat/recruit">RECRUIT</a></li>
+          <div id="intro" className="works-intro">
+            <div className="works-intro__inner u-inner">
+              <div className="works-intro__txt-wrap txtWrap">
+                <h1 className="works-intro__head tit c-head01 u-poppins">works</h1>
+                <ul className="works-intro__tab tab">
+                  <li className="works-intro__item item u-poppins is-current"><a className="works-intro__link" href="https://www.m-hand.co.jp/works/">all</a></li>
+                  <li className="works-intro__item item u-poppins"><a className="works-intro__link" href="https://www.m-hand.co.jp/works/cat/service">SERVICE</a></li>
+                  <li className="works-intro__item item u-poppins"><a className="works-intro__link" href="https://www.m-hand.co.jp/works/cat/corporate">CORPORATE</a></li>
+                  <li className="works-intro__item item u-poppins"><a className="works-intro__link" href="https://www.m-hand.co.jp/works/cat/recruit">RECRUIT</a></li>
                 </ul>
               </div>
             </div>
           </div>
           
           <div id="archive" >
-            <div class="_innerbox" >
-              <ul >
-                { console.log(props.allPosts) }
+            <div className="_innerbox  u-inner" >
+              <ul >            
+                {props.allPosts.data.map((post , key) => (
+                    <li  className="works-archive__item" key={key} >
+                        <a href="https://www.m-hand.co.jp/works/maeda-corporation-recruit-site/" className="works-archive__link">
+                            <div className="works-archive__img-wrap">
+                                <div className="works-archive__img" style={{"background-image": "url(https://www.m-hand.co.jp/wp/wp-content/uploads/2021/01/eb907077085c777126270a7f324c87f4.jpg)"}}></div>
+                            </div>
+                            <div className="works-archive__area-txt">
+                                <p className="works-archive__head u-poppins">{post.title}</p>
+                            </div>                                               
+                        </a>
+                    </li>
+                ))}                
               </ul>
             </div>
           </div>

ba50c41c88d9e96f1d3ee98466de37a84d6c0dc7
fatooo0116@gmail.com <cbsi940424>
2021-11-19 13:13:16 +0800

    update

--- pages/tpl/_header.js
+++ pages/tpl/_header.js
@@ -49,15 +49,18 @@ class Header extends React.Component{
         <div id="aloha_header"   className={(this.state.menuCloseAni)? "closing":"" }>
         
           <Head>
-            <title>Aloha Tech Software</title>
+            <title>Aloha Tech Software | 軟體設計 | 網頁設計 | APP建置 </title>
 
-            <meta name="description" content="" />
-            <meta name="keywords" content="" />
+            <meta name="description" content="幫助各個企業主加速商品銷售，不論公司規模大小，我們提供各種銷售利器，讓您的訊息能迅速傳播，掌握各種商機，結合最新銷售管道像是 Line App ，FB行銷，線上直播等等，我們也能客制各種軟體ERP ,ios app android app wordpress, 各種API串接開發，簡化貴公司的事務處理流程，或者是優化網站加強SEO" />
+            <meta name="keywords" content="軟體設計,網頁設計,APP建置,台北網頁設計,台北網頁推薦,web develope,website developement, ios app,flutter," />
 
             <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
             <meta http-equiv="x-ua-compatible" content="ie=edge"></meta>
             <link rel="icon" href="/favicon.ico" />
             <link rel="stylesheet" href="/main.css" />
+
+
+            
             
             <script async src="https://www.googletagmanager.com/gtag/js?id=UA-157417377-2"></script>
             <script

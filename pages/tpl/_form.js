ed7ac5fee493768e122e95d4da8cb75e093d03f6
fatooo0116@gmail.com <cbsi940424>
2021-11-21 11:06:29 +0800

    update

--- pages/tpl/_form.js
+++ pages/tpl/_form.js
@@ -9,34 +9,12 @@ class Form extends React.Component{
       super(props)
       this.state = {
           menuOpen:0,
-          menuCloseAni:0
+          menuCloseAni:0,
+          msgContent:''
       }    
   }
 
-    /*
-    ToggleMenu = () =>{    
-        this.setState({
-        menuOpen:!this.state.menuOpen
-        });
-    }
 
-
-
-
-    MenuClose = () =>{
-        this.setState({
-        menuCloseAni:1
-        });
-
-        let me = this;
-        setTimeout(function(){
-        me.setState({
-            menuOpen:0,
-            menuCloseAni:0
-        });
-        },1100);
-    }
-    */
 
 
   registerUser = async event => {
@@ -48,7 +26,7 @@ class Form extends React.Component{
     formData.append('youremail', event.target.youremail.value);
     formData.append('yourmessage', event.target.yourmessage.value);
 
-    const res = await fetch('https://api.aloha-tech.com/wp-json/contact-form-7/v1/contact-forms/52/feedback', {
+    const res = await fetch('https://api.aloha-tech.com/wp-json/contact-form-7/v1/contact-forms/89/feedback', {
       
         body: formData,
       method: 'POST'
@@ -59,9 +37,19 @@ class Form extends React.Component{
 
     console.log(result);
 
-    this.setState({form_name:'x11'});
+    if(result.status=='mail_sent'){
+     // alert('感謝您已寄出');
+      this.setState({
+        msgContent:'感謝您訊息已寄出'
+      });
+    }else{
+      this.setState({
+        msgContent:'發生錯誤'
+      });
+    }
     
   }
+
 
 
 
@@ -69,7 +57,10 @@ class Form extends React.Component{
 
     return(
         
-        <form onSubmit={this.registerUser}>                  
+        <form  className="contact_form" onSubmit={this.registerUser}>  
+
+            {(this.state.msgContent)? <div className="w100">{this.state.msgContent}</div>:''}
+
             <input id="name" name="yourname" type="text"  placeholder="姓名"  required />
             <input id="tel" name="tel425" type="text"  placeholder="電話"  required />
             <input id="email" name="youremail" type="email"  placeholder="Email"   />            

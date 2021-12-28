8adc7511d17ba60e48fcb4f4cc78e0bc74c3e784
fatooo0116@gmail.com <cbsi940424>
2021-12-29 00:01:23 +0800

    udpate

--- pages/tpl/_footer.js
+++ pages/tpl/_footer.js
@@ -22,6 +22,7 @@ export default function Footer() {
                                 <a href="#">HOME</a>
                                 </Link>
                         </li>   
+                              
                                              
                         <li>
                                 <Link href={'/myblog'} >
